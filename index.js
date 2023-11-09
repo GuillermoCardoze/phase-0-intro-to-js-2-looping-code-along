fcb7af186f6715c8a88eb7da190ed611bb760eb4
Guillermo Cardoze <vital_tmv@yahoo.com>
2023-11-09 11:54:28 -0600

    complete

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 
 writeCards(names);
 
-function writeCards(names) {
+function writeCards(names){
   let thankYouMessages = [];
 
   for (let i = 0; i < names.length; i++) {
-    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    let message =`Thank you, ${names[i]}, for the wonderful surprise gift!`;
     thankYouMessages.push(message);
   }
-
   return thankYouMessages;
 }
 
+function countDown(number){
+  for (let i = number; i >= 0; i --)
+  console.log(i);
 
-function countDown(number) {
-  for (let i = number; i >= 0; i--) {
-    console.log(i);
-    
-  }
 }
+
+
